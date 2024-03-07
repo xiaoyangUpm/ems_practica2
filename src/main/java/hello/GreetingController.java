4af9b3b3aa0346365229e4a374af2a84542199cb
xiaoyang <xiaoyang.peng@alumnos.upm.es>
2024-03-07 17:31:24 +0100

    prueba3

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -9,6 +9,7 @@ public class GreetingController {
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World")
                            String name, Model model) {
         model.addAttribute("name", name);
+        model.addAttribute("name", name);
         return "greeting";
     }
 }
